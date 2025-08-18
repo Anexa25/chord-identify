12acf1fed0bc69b629e5b548f71ebf04b591cbe1
Anexa25 <anexabijoy5@gmail.com>
2025-08-18 17:00:35 +0100

    inital commit

---
+++ main.py
@@ -0,0 +1,16 @@
+import tkinter as tk
+
+root = tk.Tk()
+
+root.title("Chord identifier")
+root.configure(background="green")
+root.minsize(300, 300)
+root.maxsize(500, 500)
+
+tk.Label(root, text="Nothing will work unless you do.").pack()
+
+
+root.mainloop()
+
+
+
